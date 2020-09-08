2a116389834714e999489d9ac762d6688626704d
Natalia <nataliarudomyr1@gmail.com>
2020-09-08 13:30:58 +0300

    hw15

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Post from'./components/posts';
 import './App.css';
+
+
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+const postList = [
+  {
+    name: "Anakin skywalker",
+    photo: <img id="photo" src={ANAKIN_IMAGE} alt="Anakin"/>,
+    nickname: "@dart_vader",
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: <img id="image" src={RAY_IMAGE} alt="Ray"/>,
+    date: "26 лют."
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { postList.map(post => <Post {...post}/>)}
     </div>
   );
 }
